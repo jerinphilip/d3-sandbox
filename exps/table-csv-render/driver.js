3c8e34b48aba68a557970aeeb74968c0d350c629
Jerin Philip <jerin.philip@research.iiit.ac.in>
2018-05-30 14:37:30 +0530

    More data added. d3 update enabled.

--- exps/table-csv-render/driver.js
+++ exps/table-csv-render/driver.js
@@ -1,14 +1,8 @@
-
-d3.csv('table.csv', function(data){
-
-    var hist_fn = d3.histogram().value(d => d.bleu).thresholds(20);
-    var bins = hist_fn(data);
-    var avg_bleu = d3.sum(data, d => d.bleu)/data.length;
-    console.log(avg_bleu);
-    bins.reverse();
-
+var update_text = function(bins){
     var universal = d3.select("#content")
         .selectAll("div")
+        .remove()
+        .exit()
         .data(bins)
         .enter()
 
@@ -43,30 +37,31 @@ d3.csv('table.csv', function(data){
     add(text, 'source', d => d.x);
     add(text, 'target', d => d.z);
     add(text, 'prediction', d => d.y);
+}
 
+var update_histogram = function(bins) {
     var x = d3.scaleLinear().rangeRound([0, width]);
     var y = d3.scaleLinear()
         .domain([0, d3.max(bins, d => d.length)])
         .range([height, 0]);
-
-
     var _translate = (_x, _y) => 'translate(' + x(_x) + ',' + y(_y) + ")"
+    var _dx = d => x(d.x1) - x(d.x0)
+    var binwidth = width/bins.length;
+
+    var _debug = function(d){
+        return d.x0;
+    }
 
     var bar = g.selectAll(".bar")
+        .remove()
+        .exit()
         .data(bins)
         .enter().append("g")
         .attr("class", "bar")
         .attr("transform", d => _translate(d.x0, d.length))
-
-    var _dx = d => x(d.x1) - x(d.x0)
-
-    var debug = function(d){
-        console.log(d)
-    }
-
     bar.append("rect")
-        .attr("x", 1)
-        .attr("width", _dx(bins[0]) - 1)
+        .attr("x", _debug) 
+        .attr("width", binwidth)
         .attr("height", d => height - y(d.length))
         .attr("fill", "steelblue")
 
@@ -80,9 +75,26 @@ d3.csv('table.csv', function(data){
         .attr("class", "axis axis--x")
         .attr("transform", _translate(0, 0))
         .call(d3.axisBottom(x))
+}
 
-    d3.select("#avg-bleu")
-        .text(avg_bleu);
+var load_summary = function(dataset){
+    var path = 'csvs/translation-'+ dataset + '.csv'
+    console.log(path);
+
+    d3.csv(path, function(data){
+        var hist_fn = d3.histogram().value(d => d.bleu).thresholds(20);
+        var bins = hist_fn(data);
+        var avg_bleu = d3.sum(data, d => d.bleu)/data.length;
+        console.log(avg_bleu);
+        bins.reverse();
+        update_text(bins);
+        update_histogram(bins);
+
+        d3.select("#avg-bleu")
+            .text(avg_bleu);
+
+    });
+}
 
 
-});
+load_summary("ipl");
