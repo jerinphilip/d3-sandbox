383fca86cf4d6ddafb0bde6775917db76155ae82
Jerin Philip <jerin.philip@research.iiit.ac.in>
2019-11-14 13:04:22 +0530

    minor changes

--- exps/uc-viz/main.js
+++ exps/uc-viz/main.js
@@ -1,6 +1,6 @@
 
-function random_sample(arr, size) {
-    var shuffled = arr.slice(0), i = arr.length, temp, index;
+var random_sample = function(population, size) {
+    var shuffled = population.slice(0), i = population.length, temp, index;
     while (i--) {
         index = Math.floor((i + 1) * Math.random());
         temp = shuffled[index];
@@ -9,7 +9,6 @@ function random_sample(arr, size) {
     }
     return shuffled.slice(0, size);
 }
-
 
 var accessor = function(x, key_sequence){
     for(var i=0; i < key_sequence.length; i++){
@@ -23,10 +22,8 @@ var transform = function(json, invert, key_sequence){
     var src  = json["src"].split(" ");
     var tgt  = json["tgt"].split(" ")
     var hyp  = json["hyp"].split(" ")
-
     var scores = accessor(json, key_sequence);
     var scale = (invert)? -1: 1;
-
     ucs = [];
     for(var i=0; i < hyp.length; i++){
         uc = {
@@ -46,6 +43,7 @@ var transform = function(json, invert, key_sequence){
     return data;
 };
 
+
 var operate = function(root, json, invert, score_f){
     var _min, _max;
     data = transform(json, invert, score_f);
@@ -53,8 +51,6 @@ var operate = function(root, json, invert, score_f){
     _max = d3.max(data.ucs, d => d.score);
     color = d3.scaleSequential(d3.interpolateRdYlGn).domain([_min, _max]);
     ucdiv = root.append("div").attr("class", "uc")
-
-
     ucdiv
       .selectAll(".bar")
       .data(data.ucs)
@@ -66,23 +62,27 @@ var operate = function(root, json, invert, score_f){
     ucdiv.append("span").text(" (" +data.keys + ")");
 }
 
-var f = function(d){
-    console.log(d["vanilla"]["word"]["max_prob"]);
-    return g(d);
-}
-
 var display = function(root, data){
     var div = root.append("div").attr("class", "example");
     var invert = true;
     var dont_invert = false;
-    div.append("div").text("[src] " + data["src"]);
-    div.append("div").text("[tgt] " + data["tgt"]);
+
+    var clean = function(s){
+        s = s.replace(" ", "")
+        return s;
+    };
+
+    // Append src and tgt
+    div.append("div").text("[src] " + clean(data["src"]));
+    div.append("div").text("[tgt] " + clean(data["tgt"]));
+
+    // Operate and append hypothesis
     var ucs = div.append("div").attr("class", "ucs");
     operate(ucs, data, dont_invert, ["vanilla", "word", "max_prob"]);
     operate(ucs, data, dont_invert, ["epistemic", "word", "expectation"]);
-    operate(ucs, data, invert, ["vanilla", "word", "entropy"]);
-    operate(ucs, data, invert, ["aleatoric", "word"]);
-    operate(ucs, data, invert, ["epistemic", "word", "variance"]);
+    operate(ucs, data,      invert, ["vanilla", "word", "entropy"]);
+    operate(ucs, data,      invert, ["aleatoric", "word"]);
+    operate(ucs, data,      invert, ["epistemic", "word", "variance"]);
 }
 
 d3.json("./output.json").then(function(json) {
